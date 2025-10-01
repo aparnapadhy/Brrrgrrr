f1122b03a043232ac8489137a9f54e023470b70f
aparnapadhy <aparnapadhyap672@gmail.com>
2025-10-01 11:38:43 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,7 +10,7 @@ const StoreContextProvider = (props) => {
 
   const [token, setToken] = useState("")
   
-  const url = "http://localhost:4000"
+  const url = "https://brrrgrrr-backend-ggfd.onrender.com"
 
   const [food_list, setFoodList] = useState([])
 
